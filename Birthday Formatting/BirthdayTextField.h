b408d0ec343c40404bdf435d9367769e92f98975
wanted <quentin.rubini@easylia.com>
2015-04-13 12:12:13 +0200

    Create UITextField subclass + force number pad keyboard

---
+++ Birthday Formatting/BirthdayTextField.h
@@ -0,0 +1,13 @@
+//
+//  BirthdayTextField.h
+//  Birthday Formatting
+//
+//  Created by Quentin Rubini on 13/04/2015.
+//  Copyright (c) 2015 Jason Loewy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BirthdayTextField : UITextField <UITextFieldDelegate>
+
+@end
